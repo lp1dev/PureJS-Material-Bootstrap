62b432fcef1317b5b686de422a8e73d5649aab5c
Lupin <mail@lupin.me>
2015-12-02 13:56:18 +0100

    refactored drawer

--- res/js/drawer.js
+++ res/js/drawer.js
@@ -1,11 +1,19 @@
 var isDrawerOpened = false;
-var mainContainer, navigationDrawer, filter;
+var mainContainer, navigationDrawer, filter, header;
 document.addEventListener("DOMContentLoaded", function(event) {
     mainContainer = document.getElementById("mainContainer");
     navigationDrawer = document.getElementById("drawer");
+    header = document.getElementById("headerContent");
     filter = document.getElementById("filter");
     mainContainer.style.marginLeft = 0;
+    header.style.marginLeft = 0;
 });
+
+function isMobile(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
+	return true
+    return false;
+}
 
 function drawer(){
     if (isDrawerOpened)
@@ -15,14 +23,25 @@ function drawer(){
     isDrawerOpened = !isDrawerOpened;
 }
 
+
 function openDrawer(){
-    mainContainer.style.marginLeft = navigationDrawer.style.width;
-    navigationDrawer.className = "drawer shadowed";
-    filter.style.opacity = 0.5;
+    navigationDrawer.className = "drawer";
+    if (isMobile()){
+	filter.style.visibility = "visible";
+    }
+    else{
+	mainContainer.style.marginLeft = navigationDrawer.style.width;
+	header.style.marginLeft = navigationDrawer.style.width;
+    }
 }
 
 function closeDrawer(){
-    mainContainer.style.marginLeft = 0;
     navigationDrawer.className = "drawer shadowed closed";
-    filter.style.opacity = 0;
+    if (isMobile()){
+	filter.style.visibility = "hidden";	
+    }
+    else{
+	mainContainer.style.marginLeft = 0;
+	header.style.marginLeft = 0;
+    }
 }
